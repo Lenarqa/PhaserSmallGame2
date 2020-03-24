638a56c3a0e45c1dd6f9aa886558302d99dc292f
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2020-03-24 23:42:04 +0700

    Create config variable, add images, add background

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 console.log("Hello from main.js");
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 400,
+    scene:{
+        preload: preload,
+        create: create,
+        update: update
+    }
+
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    //image
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+
+    //spritesheet
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48});
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+}
+
+function update ()
+{
+
+}
